6602233b8ea736c96ebf83a1f0bc7a88773d1a5e
mintaq <minh.tranquang98@gmail.com>
2022-08-01 22:48:39 +0700

    Update pong game

--- 100-days-of-code/day-22/pong-game/ball.py
+++ 100-days-of-code/day-22/pong-game/ball.py
@@ -10,6 +10,8 @@ class Ball(Turtle):
     def __init__(self, position):
         super().__init__()
         self.create_ball(position)
+        self.x_move = 10
+        self.y_move = 10
 
     def create_ball(self, position):
         self.shape("circle")
@@ -19,21 +21,23 @@ class Ball(Turtle):
         self.speed("fastest")
         self.shapesize(stretch_wid=1, stretch_len=1)
 
-    def move(self, directions=(5, 3)):
-        new_x = self.xcor() + directions[0]
-        new_y = self.ycor() + directions[1]
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def new_direction(self):
-        if self.heading() == RIGHT:
-            return (-5, +3)
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
 
     def is_collision_with_paddle(self, paddle, max_distance=10):
-        print(self.distance(paddle) < max_distance)
         return self.distance(paddle) < max_distance
 
-    def is_collision_with_wall(self, wall_border=290):
-        return self.xcor() > wall_border or \
-            self.xcor() < -wall_border or \
-            self.ycor() > wall_border or \
-            self.ycor() < -wall_border
+    def is_collision_with_wall(self, wall_border=280):
+        return self.ycor() > wall_border or \
+               self.ycor() < -wall_border
+
+    def reset_position(self):
+        self.goto(0, 0)
--- 100-days-of-code/day-22/pong-game/main.py
+++ 100-days-of-code/day-22/pong-game/main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.bgcolor("black")
 screen.title("My Pong Game")
 screen.tracer(0)
 
+scoreboard = Scoreboard()
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball((0, 0))
@@ -25,8 +27,24 @@ ball_directions = (5, -1)
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    ball.move(ball_directions)
-    if ball.is_collision_with_paddle(r_paddle):
-        ball_directions = ball.new_direction()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.is_collision_with_wall():
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or \
+            ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect miss
+    if ball.xcor() > 380:
+        scoreboard.l_point()
+        ball.reset_position()
+
+    if ball.xcor() < -380:
+        scoreboard.r_point()
+        ball.reset_position()
 
 screen.exitonclick()
