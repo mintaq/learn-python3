9f0672addb6f0ac39393ffc5aba121e4ae51a8d3
mintaq <minh.tranquang98@gmail.com>
2022-09-07 21:09:56 +0700

    Update mail merge project

--- 100-days-of-code/day-24/Mail Merge Project Start/main.py
+++ 100-days-of-code/day-24/Mail Merge Project Start/main.py
@@ -4,15 +4,19 @@
 #Save the letters in the folder "ReadyToSend".
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+PLACEHOLDER_NAME = "[name]"
+
 with open("./Input/Names/invited_names.txt", "r") as invited_names_file:
-    with open("./Input/Letters/starting_letter.txt") as starting_letter_file:
-        names = invited_names_file.readlines()
-        starting_letter = starting_letter_file.read()
-        for name in names:
-            starting_letter_cp = starting_letter
-            name = name.strip('\n')
-            starting_letter_cp = starting_letter_cp.replace("[name]", name)
-            with open(f"./Output/ReadyToSend/mail_{name}", "w") as output_mail_file:
-                output_mail_file.writelines(starting_letter_cp)
-        #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-            #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+    names = invited_names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as starting_letter_file:
+    starting_letter = starting_letter_file.read()
+    for name in names:
+        starting_letter_cp = starting_letter
+        name = name.strip('\n')
+        starting_letter_cp = starting_letter_cp.replace(PLACEHOLDER_NAME, name)
+        with open(f"./Output/ReadyToSend/mail_{name}.txt", "w") as output_mail_file:
+            output_mail_file.writelines(starting_letter_cp)
