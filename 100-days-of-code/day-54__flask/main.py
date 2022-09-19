c66f8270ad87fcb26be10d3e8b20315a9076cb50
mintaq <minh.tranquang98@gmail.com>
2022-09-19 21:28:53 +0700

    update

--- 100-days-of-code/day-54__flask/main.py
+++ 100-days-of-code/day-54__flask/main.py
@@ -3,6 +3,23 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+
+    return wrapper
+
+
 @app.route("/")
+@make_bold
 def hello_world():
     return "Hello world"
+
+
+@app.route("/<name>")
+def greet(name):
+    return f"Hello the one and only: {name}!"
+
+
+if __name__ == "__main__":
+    app.run()
